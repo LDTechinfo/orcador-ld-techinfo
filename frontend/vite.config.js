f010d752f76ac76630a90fd5435dfb08a59bcd42
Leo - L&D Techinfo <techinfo.led@gmail.com>
2025-11-07 00:21:29 -0300

    Atualização frontend para deploy Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'  // garante que a build vai para a pasta esperada pelo Vercel
+    outDir: 'dist' // pasta esperada pelo Vercel
   },
-  base: './' // garante que os assets funcionem mesmo em subpastas ou deploys estáticos
+  base: './' // garante que assets funcionem em subrotas
 })
